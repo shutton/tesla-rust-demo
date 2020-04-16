d56f2559ff60fd41a0b3195c8cfc8c431ae98896
Scott Hutton <schutton@cisco.com>
2020-04-16 09:53:43 -0700

    Convert main() to async

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use log::*;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     pretty_env_logger::formatted_timed_builder()
         .filter(Some("trd"), log::LevelFilter::Info)
         .filter(None, log::LevelFilter::Warn)
